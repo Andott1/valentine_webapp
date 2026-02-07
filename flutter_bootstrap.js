2e0f45cdf169b219483f8ea2000297614fec85da
Andott1 <Andott1@users.noreply.github.com>
2026-02-07 06:59:45 +0000

    deploy: f30fb0d3e810343491f787b3ab68b1ce89213302

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4290859591"
+    serviceWorkerVersion: "2750904108"
   }
 });
